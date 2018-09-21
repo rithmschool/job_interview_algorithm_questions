695983c4f36c19cf0264f1b8c0b2f6aec5495862
Mark Suzuki <markdsuzuki@gmail.com>
2018-09-21 15:38:18 -0700

    index created

---
+++ binary-search-tree-find-max-depth/index.js
@@ -0,0 +1,11 @@
+const { BinarySearchTree, BSTNode } = require("../_starters/binary-search-tree");
+
+/**
+ * @param {BSTNode} 
+ * @return {number}
+ */
+BinarySearchTree.prototype.maxDepth = function(BSTNode) {
+  
+}
+
+module.exports = { BinarySearchTree };
